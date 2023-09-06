b33feaab2e1653687ecbcd8f2ec905dbea152eb9
Hilcbek <balemayehu07@gmail.com>
2023-09-06 19:55:33 +0300

    added backend new 2

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let app = express();
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({ origin : ['http://localhost:5173'], credentials : true }))
+app.use(cors({ origin : ['https://frontend-yc6b.vercel.app','http://localhost:5173'], credentials : true }))
 app.use(helmet())
 helmet.crossOriginResourcePolicy({policy : 'same-site'})
 dotenv.config()
